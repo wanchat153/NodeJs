ed69b7ec45b82cf43bc1fb18446dd5138b893cf8
M153 <wanchatgame@gmail.com>
2023-09-13 13:51:06 +0700

    Html Hello World

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const chalk = require('chalk');
 const morgan = require('morgan');
 const app = express();
 const port = 3000;
+const path = require('path');
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/public/")));
 
 app.get("/", (req, res) => {
     res.send("Hello World");
