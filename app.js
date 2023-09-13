d28a3f36a5066db741d3fb66490388c2df5234cb
M153 <wanchatgame@gmail.com>
2023-09-13 13:39:39 +0700

    Express
    Install
    - debug
    - chalk color
    $env:DEBUG='app';node app.js
    - morgan

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const debug = require('debug')('app');
+const chalk = require('chalk');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(port, () => {
+    debug("Listening on port " + chalk.green(port));
+});
