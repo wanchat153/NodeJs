ccf0f3d22c8262961202a2467ef53a3dba0c04f5
M153 <wanchatgame@gmail.com>
2023-09-13 15:26:56 +0700

    Routing
    การ Navigate ระหว่างเว็บ และ สร้าง Router

--- app.js
+++ app.js
@@ -5,12 +5,18 @@ const morgan = require('morgan');
 const app = express();
 const PORT = process.env.PORT || 4000; //ถ้า 3000 ใช้ไม่ได้จะใช้ 4000
 const path = require('path');
+const productRouter = express.Router();
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname,"/public/")));
 
 app.set("views","./src/views");
 app.set("view engine", "ejs");
+
+app.use("/products", productRouter);
+productRouter.route("/").get((req,res)=>{
+    res.send("Products");
+})
 
 app.get("/", (req, res) => {
     res.render('index', {username: 'M', customers: ["A","B","C"]});
